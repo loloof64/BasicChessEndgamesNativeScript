31d16cb447651a53131149134350b7f69a299e79
Laurent Bernabe <laurent.bernabe@gmail.com>
2019-10-01 02:18:47 +0200

    Starting to manage DragAndDrop for piece move.

--- app/components/Home.vue
+++ app/components/Home.vue
@@ -9,7 +9,7 @@
                 width="0" height="0"
                 @loadFinished="onWebViewLoaded"
             />
-            <Chessboard :size="boardWidth" :reversed="false"></Chessboard>
+            <Chessboard :size="boardWidth" :reversed="true"></Chessboard>
         </DockLayout>
     </Page>
 </template>
